9e8fafa027cc2ce222b2bce94f439351475ec0c7
Nirbhay Singh <nirbhaysinghbe1@gmail.com>
2023-09-25 00:02:55 +0530

    wip: backend error handling and edge case

--- Backend/controllers/booksController.js
+++ Backend/controllers/booksController.js
@@ -1,26 +1,72 @@
 import express from "express";
 import { Book } from "../models/Book";
+import multer from "multer";
+const storage = multer.memoryStorage(); // Store the image in memory
+
 
 const router = express.Router();
 
-// Define routes for book-related operations
-router.post('/', async (req, res) => {
-	try {
 
-		const title = req.body.title;
-		const matchCaseTitle = Book.find({ "title": title });
-		if (matchCaseTitle) {
-			res.status(200).json("Book with the given title already exists.")
+// Define a function to check if the file is an image based on its MIME type
+const imageFileFilter = (req, file, cb) => {
+	if (file.mimetype.startsWith('image/')) {
+		return cb(null, true);
+	}
+	cb(new Error('Not an image! Please upload an image file.'), false);
+};
+
+// Define a function to check the size of the uploaded image
+const imageFileSizeLimit = 5 * 1024 * 1024; // 5 MB (adjust as needed)
+
+const upload = multer({
+	storage,
+	fileFilter: imageFileFilter,
+	limits: { fileSize: imageFileSizeLimit }, // Set the size limit
+});
+
+
+// Define routes for book-related operations
+router.post('/', upload.single('image'), async (req, res) => {
+	try {
+		// Check if the image file was provided
+		if (!req.file) {
+			return res.status(400).json({ error: 'Image is required' });
 		}
 
-		const newBook = new Book(req.body);
+		// Check if other required fields are provided
+		const { title, authors, price } = req.body;
+		if (!title) {
+			return res.status(400).json({ error: 'Title is required' });
+		}
+		if (!authors) {
+			return res.status(400).json({ error: 'Authors are required' });
+		}
+		if (!price) {
+			return res.status(400).json({ error: 'Price is required' });
+		}
+
+		// Check if a book with the same title already exists
+		const existingBook = await Book.findOne({ title });
+		if (existingBook) {
+			return res.status(409).json({ error: 'A book with the same title already exists' });
+		}
+
+		// Create a new book document with the provided data
+		const newBook = new Book({
+			...req.body,
+			image: req.file.buffer // Store the image as binary data
+		});
+
+		// Save the new book to the database
 		await newBook.save();
+
 		res.status(201).json(newBook);
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Failed to add the book.' });
 	}
 });
+
 
 // Define the route for searching books with separate criteria
 router.get('/search', async (req, res) => {
--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -6,6 +6,7 @@ import { secret } from '../config';
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
+
 passport.use(
 	new JWTStrategy(
 		{
@@ -13,6 +14,7 @@ passport.use(
 			secretOrKey: secret.key,
 		},
 		(jwtPayload, done) => {
+
 			return done(null, jwtPayload.userId);
 		}
 	)
